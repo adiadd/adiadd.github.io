19082acdad807dcbfe229d8d1f7ceb241c182671
Aditya Addepalli <adi.addepalli@gmail.com>
2024-11-06 10:19:26 -0500

    change: changing title to backslash

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,7 +15,7 @@ export const metadata: Metadata = {
   metadataBase: new URL(baseUrl),
   title: {
     default: siteConfig.name,
-    template: `%s | ${siteConfig.nickName}`,
+    template: `%s / ${siteConfig.nickName}`,
   },
   icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✌🏾</text></svg>",
   description: siteConfig.description,
