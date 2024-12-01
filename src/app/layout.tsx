fd563861338f2a9b768f234d7c823c670a99edf3
Aditya Addepalli <adi.addepalli@gmail.com>
2024-12-01 13:53:28 -0600

    format: running prettier

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,8 @@ export const metadata: Metadata = {
     default: siteConfig.name,
     template: `%s / ${siteConfig.nickName}`,
   },
-  icons: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✌🏾</text></svg>",
+  icons:
+    "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>✌🏾</text></svg>",
   description: siteConfig.description,
   openGraph: {
     title: siteConfig.name,
--- src/components/cal-button.tsx
+++ src/components/cal-button.tsx
@@ -1,4 +1,4 @@
-'use client';
+"use client";
 
 import { getCalApi } from "@calcom/embed-react";
 import { useEffect } from "react";
