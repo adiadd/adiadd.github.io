e4e0f3aa0d93b62bf7222829fadb8ba476008d27
Aditya Addepalli <adi.addepalli@gmail.com>
2025-02-18 12:04:13 -0500

    fix: fixing rotating pfp 2

--- src/components/client/profile-image.tsx
+++ src/components/client/profile-image.tsx
@@ -7,23 +7,19 @@ export function ProfileImage() {
   const [isLoading, setIsLoading] = useState(true);
 
   return (
-    <div className="w-14 h-14 relative">
-      <div
-        className="w-full h-full rounded-full overflow-hidden transform-gpu hover:animate-spin"
-        style={{ transformOrigin: "center center", contain: "paint" }}
-      >
-        {isLoading && (
-          <div className="absolute inset-0 animate-pulse bg-gray-200 dark:bg-gray-700" />
-        )}
-        <Image
-          src="https://utfs.io/f/X8OJl8spEec683HrXmNyqLUu2H0ri7xZDjQtWpBhGEVIcdeR"
-          alt="Real Aditya"
-          fill
-          sizes="100vw"
-          style={{ objectFit: "cover" }}
-          onLoad={() => setIsLoading(false)}
-        />
-      </div>
+    <div className="w-14 h-14 rounded-full relative overflow-hidden group cursor-pointer">
+      {isLoading && (
+        <div className="absolute inset-0 animate-pulse bg-gray-200 dark:bg-gray-700" />
+      )}
+      <Image
+        src="https://utfs.io/f/X8OJl8spEec683HrXmNyqLUu2H0ri7xZDjQtWpBhGEVIcdeR"
+        alt="Real Aditya"
+        fill
+        sizes="100vw"
+        style={{ objectFit: "cover" }}
+        onLoad={() => setIsLoading(false)}
+        className="transition-transform duration-1000 group-hover:rotate-360"
+      />
     </div>
   );
 }
