5bb2208c865bf888482f321a5e7e942a35aaa72a
Aditya Addepalli <adi.addepalli@gmail.com>
2025-06-18 12:29:55 -0400

    feat: add applied ai interest section to personal page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,10 +16,12 @@ export default function Page() {
       </h2>
       <h2 className="my-4 text-lg font-semibold tracking-tighter">📍 nyc</h2>
       <p className="mb-4 text-neutral-900 dark:text-neutral-100">
-        heyo ✌🏾! my name is aditya <i> [ah DITH ya]</i>. i love to learn and build.
+        heyo ✌🏾! my name is aditya <i> [ah DITH ya]</i>. i love to learn and
+        build.
       </p>
       <p className="mb-4 text-neutral-900 dark:text-neutral-100">
-        i previously graduated from <span style={{ color: "#bf5700" }}>UT Austin</span> with a major in
+        i previously graduated from{" "}
+        <span style={{ color: "#bf5700" }}>UT Austin</span> with a major in
         information systems and a certificate in cs. during the day, i write
         code for Amazon Web Services, at night, for the master of computer
         science from <span style={{ color: "#B3A369" }}>Georgia Tech</span>{" "}
@@ -28,6 +30,19 @@ export default function Page() {
       <p className="mb-4 text-neutral-900 dark:text-neutral-100">
         i&apos;m a curious person by nature and one of my life&apos;s goal is to
         eventually make the world a better place, in whatever way that may be.
+      </p>
+      <p className="mb-4 text-neutral-900 dark:text-neutral-100">
+        i&apos;m currently extremely interested in applied ai and ai
+        engineering.
+      </p>
+      <p className="mb-4 text-neutral-900 dark:text-neutral-100">
+        i think the world will be drastically different in the next 5, 10, 20+
+        years. ai will not be an assistant or agent(s) but deeply integrated
+        into our society in ways we can&apos;t even imagine. i want to
+        contribute to a world where society is empowered by ai, where humans are
+        free in their thinking. to think big, solve major & quixotic problems,
+        and further humanity as a whole; ai to be an enhancer not an obstacle or
+        something to fear.
       </p>
       <div>
         <p className="text-neutral-900 dark:text-neutral-100">
