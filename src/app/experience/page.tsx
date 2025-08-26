291cf0556e60608d15f7ce76eb09a09144afcc41
Aditya Addepalli <adi.addepalli@gmail.com>
2025-08-26 12:55:50 -0400

    fix: update job title in experience page

--- src/app/experience/page.tsx
+++ src/app/experience/page.tsx
@@ -12,7 +12,7 @@ export default function Page() {
         <TypeWriter text="my experiences" delay={100} />
       </h1>
       <h2 className="mb-2 text-lg font-semibold tracking-tighter">
-        senior cloud engineer @{""}
+        software engineer, gen ai @
         <strong>
           <a
             href="https://aws.amazon.com/"
